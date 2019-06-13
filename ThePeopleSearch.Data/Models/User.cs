c49dc3c64032e39d84a955117937f3ff636506de
Mitchell Console <mitchell.console@gmail.com>
2019-06-12 21:47:36 -0400

    Added User model to .Data project

---
+++ ThePeopleSearch.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePeopleSearch.Data.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public int age { get; set; }
+        public string interests { get; set; }
+        public string image { get; set; }
+    }
+}
